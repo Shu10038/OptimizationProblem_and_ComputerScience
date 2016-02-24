3e62592be0f6b540a14f5a92b95fdd85595fb83f
shu10038 <shuichi.ohsawa.00@gmail.com>
2016-02-24 20:09:23 +0900

    Fix it

--- LP_solver_test/LP_solver_test/LP_solver_test.cpp
+++ LP_solver_test/LP_solver_test/LP_solver_test.cpp
@@ -2,21 +2,24 @@
 #include<fstream>
 #include<iostream>
 #include<math.h>
-#include"LP_SOLVER01.h"
+#include"LP_SOLVER04.h"
 using namespace std;
 
 int main(){
 	int i, j, k;
 
-	double objective[2] = { -2.0, -3.0 };
-	double leftSide[6] = { 1.0, 2.0, 1.0, 1.0, 3.0, 1.0 };
-	double rightSide[3] = { 14.0, 8.0, 18.0 };
-	double optimumSolution[2];
+	double objective[3] = { 1.0, 2.0 ,3.0 };
+	double leftSide[9] = { 1.0, 0.0,1.0 , 2.0, 1.0,2.0, 3.0, 1.0, 2.0 };
+	double rightSide[3] = { 2.0, 5.0, 6.0 };
+	double optimumSolution[3];
+	double result;
 
-
-	LP_SOLVER01(2, 3, 0, objective, leftSide, rightSide, optimumSolution);
-	cout << "x1 = " << optimumSolution[0] << "  x2 = " << optimumSolution[1] << endl;
-	cout << "Å‘å’l‚Í " << objective[0] * optimumSolution[0] + objective[1] * optimumSolution[1] << endl;
+	result = LP_SOLVER04(sizeof objective / sizeof(double), sizeof rightSide / sizeof(double), 0, objective, leftSide, rightSide, optimumSolution);
+	for (i = 0; i < sizeof rightSide / sizeof(double); i++){
+		cout << "x"<<i+1 <<" = " << optimumSolution[i] << " ," ;
+	}
+	cout << endl;
+	cout << "Å“K’l‚Í " << result << endl;
 
 	return 0;
 }
